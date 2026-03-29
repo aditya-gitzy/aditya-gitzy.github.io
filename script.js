c714ce55b808cbebe5fb603a99f9a42bbe6a668e
Aditya Lande <landeaditya91@gmail.com>
2026-03-29 21:40:52 +0530

    Refactor smooth scrolling and update console message
    Updated console message to reflect new era.

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-// Smooth Scrolling
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-
     anchor.addEventListener('click', function (e) {
-
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
-
             behavior: 'smooth'
-
         });
-
     });
-
 });
 
-
-
-// Simple Console Message
-
-console.log("Portfolio loaded successfully. Welcome, Developer.");
+console.log("Portfolio loaded successfully. Welcome to the new era.");
